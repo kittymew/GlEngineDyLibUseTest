65e35d0ea8059a7f0f856f2bf2396fd75924c28d
강호인 <hoink@aceproject.co.kr>
2022-11-07 00:56:08 +0900

    첫 커밋

---
+++ glEngineDyProject/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  glEngineDyProject
+//
+//  Created by Hoin Kang on 2022/11/04.
+//
+
+#include <iostream>
+#include <GLEngine.h>
+#include <Object.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
